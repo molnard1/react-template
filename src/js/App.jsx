0543dd4085207784d4f47651ea38bdb67f5c40c9
molnard1 <101341428+molnard1@users.noreply.github.com>
2023-10-16 11:38:01 +0200

    init

---
+++ src/js/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+export default function App() {
+    return [
+		<h1>Hello World!</h1>
+	]
+}
+
+createRoot(document.getElementById("root")).render(<App />);
