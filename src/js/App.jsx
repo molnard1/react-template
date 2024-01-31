5f38bdbe5dfa61bce029e31a1b1801a78d33d865
molnard1 <101341428+molnard1@users.noreply.github.com>
2024-01-31 10:43:53 +0100

    Change return to use parenthesis
    Apparently using square brackets can cause some issues...

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 
 export default function App() {
-    return [
+    return (
 		<h1>Hello World!</h1>
-	]
+	)
 }
 
 createRoot(document.getElementById("root")).render(<App />);
